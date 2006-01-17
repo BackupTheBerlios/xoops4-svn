5f252741cf70cffed52518df160c48b075e7c2f1
skalpa <skalpa@8fd21cda-de02-0410-8884-89147d1a766c>
2006-01-17 17:12:06 +0000

    Tagging the released 2.3.0-a1 files

---
+++ XoopsCore/releases/2.3.0-a1/htdocs/themes/xoops-devteam/xo-info.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+return array(
+	'xoBundleIdentifier'	=> 'theme_xoops__devteam',
+	'xoBundleDisplayName'	=> 'devteam.xoops.org theme',
+	'parentTheme'			=> 'default',
+	// This theme default templates
+	// templates paths must start with a "." to be considered relative to the theme folder
+	'canvasTemplate' => './canvas-default.xotpl',
+	'pageTemplate' => './page-default.xotpl',
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
