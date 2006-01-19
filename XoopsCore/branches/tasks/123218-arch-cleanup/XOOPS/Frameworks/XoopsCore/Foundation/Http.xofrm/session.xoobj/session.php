ec50ac3e295524f1328e0058c6d79ae3fbc8c024
skalpa <skalpa@8fd21cda-de02-0410-8884-89147d1a766c>
2006-01-19 02:52:11 +0000

    Added the default session component.
    This component uses the configuration defined in PHP, and automatically initialize the save handler in one is specified. Only additional feature: it reads a variable defined in the session, so cache limiting headers can be set individually by each user.

---
+++ XoopsCore/branches/tasks/123218-arch-cleanup/XOOPS/Frameworks/XoopsCore/Foundation/Http.xofrm/session.xoobj/session.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * xoops_http_SessionService main class file
+ *
+ * See the enclosed file LICENSE for licensing information.
+ * If you did not receive this file, get it at http://www.fsf.org/copyleft/gpl.html
+ *
+ * @copyright	The XOOPS project http://www.xoops.org/
+ * @license		http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author		Skalpa Keo <skalpa@xoops.org>
+ * @since		2.3.0
+ * @package		xoops_http
+ * @subpackage	xoops_http_Session
+ * @version		$Id$
+ */
+
+/**
+ * This file cannot be requested directly
+ */
+if ( !defined( 'XOOPS_PATH' ) ) exit();
+
+/**
+ * xoops_http_SessionService main class
+ * 
+ * The default session service provides basic session management functionality.
+ * It uses the settings defined in the PHP configuration files, and thus is not really
+ * user-configurable (you must use xoops_http_CustomSessionService for that).
+ * 
+ * Only cache limiting has been enhanced: this component allows to configure this setting
+ * on a user basis (if the 'cacheLimiter' variable is stored in the session, it will be used)
+ */
+class xoops_http_SessionService {
+	/**
+	* Save handler to use for reading/writing session data
+	* @var mixed
+	*/
+	var $saveHandler = '';
+	/**
+	 * Identifier of the current session
+	 * @var string
+	 */
+	var $sessionId = 0;
+	/**
+	 * Default cache limiting policy (if different from the one defined in php.ini
+	 * @var string
+	 */
+	var $cacheLimiter = '';
+	/**
+	 * Default cache lifetime for content
+	 * @var integer
+	 */
+	var $cacheLifetime = '';
+	
+	/**
+	* Initialize the session service
+	*/
+	function xoInit( $options = array() ) {
+	 	global $xoops;
+
+		if ( $this->saveHandler && !is_object( $this->saveHandler ) ) {
+			if ( ! ( $this->saveHandler = XOS::create( $this->saveHandler ) ) ) {
+				trigger_error( "Failed to initialize the session save handler: session cannot start.", E_USER_WARNING );
+				return false;
+			}
+		}
+		if ( $this->saveHandler ) {
+	    	session_set_save_handler(
+	    		array( &$this->saveHandler, 'open' ), array( &$this->saveHandler, 'close'),
+	    		array( &$this->saveHandler, 'read'), array( &$this->saveHandler, 'write'),
+	    		array( &$this->saveHandler, 'destroy'), array( &$this->saveHandler, 'gc')
+	    	);
+		}
+		return true;
+	}
+
+	/**
+	* Create a new session
+	*/
+	function start() {
+		if ( isset($_SESSION) ) {
+		  	return true;
+		}
+		if ( !empty($this->sessionId) ) {
+			session_id( $this->sessionId );
+		}
+		if ( !$this->cacheLimiter ) {
+			$this->cacheLimiter = session_cache_limiter();
+		}
+		session_cache_limiter( 'none' );
+		session_start();
+		$this->sessionId = session_id();
+		// Do this here to allow user-dependent cacheLimiter setting
+		if ( @$var = $this->getObjectVar( $this, 'cacheLimiter' ) ) {
+			$this->cacheLimiter = $var;
+		}
+		$this->resetCacheLimiting( $this->cacheLimiter, 60 * $this->cacheLifetime );
+		return $this->sessionId;
+	}
+	/**
+	* Destroy the current session
+	*/
+	function destroy() {
+		$_SESSION = array();
+		session_destroy();
+	}
+	/**
+	* Set a named session variable
+	*/
+	function setObjectVar( $owner, $key, $value ) {
+		if ( !isset($_SESSION) ) {
+			return null;
+		}
+		if ( is_object($owner) ) {
+			$owner = $owner->xoBundleIdentifier;
+		}
+		return $_SESSION[$owner][$key] = $value;
+	}
+	/**
+	* Get the value of a named session variable
+	*/
+	function getObjectVar( $owner, $key ) {
+		if ( is_object($owner) ) {
+			$owner = $owner->xoBundleIdentifier;
+		}
+		return @$_SESSION[$owner][$key];
+	}
+	/**
+	 * Resend the cache limiting headers
+	 */
+	function resetCacheLimiting( $policy, $cacheLifetime = 0, $lastModified = null ) {
+		if ( !isset( $lastModified ) ) {
+			$lastModified = time();
+		}
+		switch ( $policy ) {
+		case 'none':
+			break;
+		case 'no-cache':
+			header( 'Expires: ' . $this->date( time() - 3600 ) );
+			header( 'Cache-Control: no-store,no-cache,must-revalidate,post-check=0,pre-check=0' );
+			break;
+		case 'private':
+			header( 'Expires: ' . $this->date( time() - 3600 ) );
+		case 'private_no_expire':
+			header( "Cache-Control: private,max-age=$cacheLifetime,pre-check=$cacheLifetime" );
+			break;
+		case 'public':
+		default:
+			header( 'Expires: ' . $this->date( time() + $cacheLifetime ) );
+			header( "Cache-Control: public,max-age=$cacheLifetime" );
+			header( "Last-Modified: " . $this->date( $lastModified ) );
+			break;
+		}
+	}
+	/**
+	 * Return an RFC compliant (RFC822 or RFC850) date string from a timestamp
+	 *
+	 * @param interger $time
+	 * @return string
+	 */	
+	function date( $time = null ) {
+        return gmdate(
+        	( ini_get('y2k_compliance') ? 'D, d M Y' : 'l, d-M-y' ) .' H:i:s',
+        	isset($time) ? $time : time()
+        ) . ' GMT';
+    }
+
+}
+
+
+?>
