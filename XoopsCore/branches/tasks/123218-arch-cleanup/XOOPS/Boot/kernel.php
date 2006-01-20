ee27979e8c869a840e0c961add8ca353289793e1
skalpa <skalpa@8fd21cda-de02-0410-8884-89147d1a766c>
2006-01-20 08:33:19 +0000

    Prepared the kernel for the authentication layer refactoring, and re-ordered a few methods

--- XoopsCore/branches/tasks/123218-arch-cleanup/XOOPS/Boot/kernel.php
+++ XoopsCore/branches/tasks/123218-arch-cleanup/XOOPS/Boot/kernel.php
@@ -80,11 +80,11 @@ class xoops_kernel_Xoops2 {
 	 */
 	var $services				= null;
 	var $captures				= array(
+		'auth'		=> 'xoops_auth_AuthenticationService',
 		'http'		=> 'xoops_http_HttpHandler',
 		'session'	=> 'xoops_http_SessionService',
 		'legacydb'	=> array( 'xoops_db_Database', array( 'driverName' => 'xoops.legacy' ) ),
 	);
-
 	/** 
 	 * Indicates whether the boot sequence has been performed or not
 	 * 
@@ -105,7 +105,43 @@ class xoops_kernel_Xoops2 {
 	 * @var string
 	 */
 	var $baseLocation			= '';
+	/**
+	 * Currently logged in user
+	 *
+	 * NOTE: This currently points to a XoopsUser instance.
+	 * THIS WILL CHANGE BEFORE THE 2.3.0 RELEASE !!
+	 * Use the $xoopsUser global if you need to access a XoopsUser
+	 * 
+	 * @var object
+	 */
+	var $currentUser			= '';
+	/**
+	 * Type of object to create when instanciating the current user (unimplemented)
+	 */
+	var $userBundleIdentifier = 'xoops_kernel_User';
 
+	/**
+	 * Whether or not we want to enable somebody to login using the system user credentials (unimplemented)
+	 *
+	 * $enableSystemUser and $systemUserProperties allow you to define a virtual user identity
+	 * that you'll be able to use to login to your site in case of problems with the current
+	 * authentication drivers or database.
+	 *
+	 * For security purposes, people are advised to disable this function, and should only enable it
+	 * when it is needed.
+	 */
+	var $enableSystemUser = false;
+	/**
+	 * Values of the properties used to build the system user object
+	 */
+	var $systemUserProperties = array(
+		'login'		=> 'xoopsadmin',
+		'password'	=> '',
+		'groups'	=> array( 1, 2 ),
+	);
+	/**
+	 * Indicates if the kernel is currently performing a virtual page request
+	 */
 	var $isVirtual				= false;
 
 	var $previousErrorHandler	= false;
@@ -130,7 +166,6 @@ class xoops_kernel_Xoops2 {
 		$this->loadRegistry();
 	 	$this->services =& $GLOBALS['exxos']->services;
 
-
 	 	$captures = @include $this->path( 'var/Application Support/xoops_kernel_Xoops2/services.php' );
 	 	if ( is_array($captures) ) {
 	 		$this->captures = array_merge( $this->captures, $captures );
@@ -145,8 +180,91 @@ class xoops_kernel_Xoops2 {
 			$this->baseLocation = '';
 		}
 		//$this->services['logger'] = $this->services['errorhandler'] = $this;
-
 	}
+
+	/**
+	 * Perform the boot sequence
+	 * 
+	 * The following operations are done in order during the boot-sequence:
+	 * - The startup-sequence file (rc.php by default) is executed
+	 * - The authenticated user object is created (if any)
+	 * - The current module is initialized
+	 * - Startup items are executed
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	function boot() {
+		if ( !$this->hasBooted ) {
+			//register_shutdown_function( array( &$this, 'shutdown' ) );
+			if ( !empty($this->bootFile) ) {
+				require_once $this->path( "/XOOPS/Boot/$this->bootFile" );
+			}
+			if ( isset( $_SESSION[$this->xoBundleIdentifier]['currentUser'] ) ) {
+				$this->acceptUser( $_SESSION[$this->xoBundleIdentifier]['currentUser'] );
+			}
+			if ( $this->launchStartupItems() ) {
+				return true;
+			} 
+			$this->hasBooted = true;
+		}
+		return true;
+	}
+	/**
+	 * Perform the shutdown sequence
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	function shutdown() {
+		if ( !$this->hasShutdown ) {
+			$this->hasShutdown = true;
+		}
+	}
+	/**
+	 * Launch the kernel startup items
+	 *
+	 * <p>Startup items are scripts than will be executed during every request.<br />
+	 * Upon completion of its normal startup sequence, the XOOPS kernel will launch every startup item
+	 * located in the StartupItems folder (by default <em>XOOPS/StartupItems</em>).</p>
+	 * <p>
+	 * To make a startup item:<br />
+	 * Create a folder in the startup items folder, then add a .php script named exactly like it inside.
+	 * </p>
+	 * <p>A startup item script can cancel the boot sequence by returning <em>false</em>.</p>
+	 * 
+	 * @access private
+	 */
+	function launchStartupItems() {
+		if ( empty( $this->startupItemsPath ) ) return true;
+		
+		$path = $this->path( $this->startupItemsPath );
+		if ( @$dh = opendir( $path ) ) {
+			while ( $file = readdir( $dh ) ) {
+				if ( $file{0} == '.' || $file == 'CVS' )	continue;
+				if ( is_dir( "$path/$file" ) ) {
+					if ( -1 == @include "$path/$file/$file.php") {
+						return false;
+					}
+				}
+			}
+			closedir( $dh );
+		}
+		return true;
+	}
+
+	function launchModule() {
+		return true;
+		if ( !$this->loadService( 'module', 'xoops_kernel_Module' ) ) {
+			return false;
+		}
+		if ( !$this->services['module']->checkAccess() ) {
+			//$this->services['http']->setResponse( 403, null, null, true );
+			return false;
+		}
+		return $this->services['module']->xoBundleIdentifier;
+	}
+
 
 	/**
 	 * Load the components registry from persistent storage
@@ -182,6 +300,30 @@ class xoops_kernel_Xoops2 {
 			$this->services[$name] =& XOS::create( $bundleId, $options );
 		}
 		return $this->services[$name];
+	}
+
+	/**
+	 * Accept the specified user as the currently logged in user
+	 *
+	 * @param string $login Login of the user to accept
+	 * @param boolean $permanent Whether to accept this user permanently or for the current request only
+	 * @return boolean
+	 */
+	function acceptUser( $login, $permanent = false ) {
+		// @TODO-2.3: Clean this up later... This handler stuff is so, so lame... :-(
+		$handler =& xoops_gethandler('member');
+	
+		list($user) = $handler->getUsers( new Criteria( 'uname', $login ) );
+		if ( is_object( $user ) ) {
+			$user->getGroups();
+			$this->currentUser = $user;
+			if ( $permanent && $this->services['session'] ) {
+				$this->services['session']->start();
+				$_SESSION[$this->xoBundleIdentifier]['currentUser'] = $login;
+			}
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -265,83 +407,6 @@ class xoops_kernel_Xoops2 {
 	}
 
 	
-	/**
-	 * Perform the boot sequence
-	 *
-	 * @access public
-	 * @return bool
-	 */
-	function boot() {
-		//$this->setupModule();
-		if ( !$this->hasBooted ) {
-			//register_shutdown_function( array( &$this, 'shutdown' ) );
-			if ( !empty($this->bootFile) ) {
-				require_once $this->path( "/XOOPS/Boot/$this->bootFile" );
-			}
-			$this->hasBooted = true;
-			if ( $this->launchStartupItems() ) {
-				return true;
-			} 
-		}
-		return true;
-	}
-
-	/**
-	 * Perform the shutdown sequence
-	 *
-	 * @access public
-	 * @return bool
-	 */
-	function shutdown() {
-		if ( !$this->hasShutdown ) {
-			$this->hasShutdown = true;
-		}
-	}
-
-	/**
-	 * Launch the kernel startup items
-	 *
-	 * <p>Startup items are scripts than will be executed during every request.<br />
-	 * Upon completion of its normal startup sequence, the XOOPS kernel will launch every startup item
-	 * located in the StartupItems folder (by default <em>XOOPS/StartupItems</em>).</p>
-	 * <p>
-	 * To make a startup item:<br />
-	 * Create a folder in the startup items folder, then add a .php script named exactly like it inside.
-	 * </p>
-	 * <p>A startup item script can cancel the boot sequence by returning <em>false</em>.</p>
-	 * 
-	 * @access private
-	 */
-	function launchStartupItems() {
-		if ( empty( $this->startupItemsPath ) ) return true;
-		
-		$path = $this->path( $this->startupItemsPath );
-		if ( @$dh = opendir( $path ) ) {
-			while ( $file = readdir( $dh ) ) {
-				if ( $file{0} == '.' || $file == 'CVS' )	continue;
-				if ( is_dir( "$path/$file" ) ) {
-					if ( -1 == @include "$path/$file/$file.php") {
-						return false;
-					}
-				}
-			}
-			closedir( $dh );
-		}
-		return true;
-	}
-
-	function launchModule() {
-		return true;
-		if ( !$this->loadService( 'module', 'xoops_kernel_Module' ) ) {
-			return false;
-		}
-		if ( !$this->services['module']->checkAccess() ) {
-			//$this->services['http']->setResponse( 403, null, null, true );
-			return false;
-		}
-		return $this->services['module']->xoBundleIdentifier;
-	}
-
 	
 } // class xoops_kernel_Xoops2
 
