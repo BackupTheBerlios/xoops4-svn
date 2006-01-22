51b30d9db39e54d1b00849ea74a438f54eb259f5
skalpa <skalpa@8fd21cda-de02-0410-8884-89147d1a766c>
2006-01-22 23:51:15 +0000

    Reverting mainfile.php (sent an installed one by mistake)

--- XoopsCore/branches/2.2.x/2.2-main/html/mainfile.php
+++ XoopsCore/branches/2.2.x/2.2-main/html/mainfile.php
@@ -24,75 +24,12 @@
 //  along with this program; if not, write to the Free Software              //
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
 //  ------------------------------------------------------------------------ //
+/**
+ * Xoops is not installed, redirect to the installer 
+ **/
 
-if ( !defined("XOOPS_MAINFILE_INCLUDED") ) {
-	define("XOOPS_MAINFILE_INCLUDED",1);
-
-	// XOOPS Physical Path
-	// Physical path to your main XOOPS directory WITHOUT trailing slash
-	// Example: define('XOOPS_ROOT_PATH', 'w:/home/xoops.local/htdocs_dev/svn-2.2.4/html');
-	define('XOOPS_ROOT_PATH', 'w:/home/xoops.local/htdocs_dev/svn-2.2.4/html');
-
-	// XOOPS Virtual Path (URL)
-	// Virtual path to your main XOOPS directory WITHOUT trailing slash
-	// Example: define('XOOPS_URL', 'http://dev.xoops.local/svn-2.2.4/html');
-	define('XOOPS_URL', 'http://dev.xoops.local/svn-2.2.4/html');
-
-	define('XOOPS_CHECK_PATH', '1');
-	// Protect against external scripts execution if safe mode is not enabled
-	if ( XOOPS_CHECK_PATH && !@ini_get('safe_mode') ) {
-		if ( function_exists('debug_backtrace') ) {
-			$xoopsScriptPath = debug_backtrace();
-			if ( !count($xoopsScriptPath) ) {
-			 	die("XOOPS path check: this file cannot be requested directly");
-			}
-			$xoopsScriptPath = $xoopsScriptPath[0]['file'];
-		} else {
-			$xoopsScriptPath = isset($_SERVER['PATH_TRANSLATED']) ? $_SERVER['PATH_TRANSLATED'] :  $_SERVER['SCRIPT_FILENAME'];
-		}
-		if ( DIRECTORY_SEPARATOR != '/' ) {
-			// IIS6 may double the \ chars
-			$xoopsScriptPath = str_replace( strpos( $xoopsScriptPath, '\\\\', 2 ) ? '\\\\' : DIRECTORY_SEPARATOR, '/', $xoopsScriptPath);
-		}
-		if ( strcasecmp( substr($xoopsScriptPath, 0, strlen(XOOPS_ROOT_PATH)), str_replace( DIRECTORY_SEPARATOR, '/', XOOPS_ROOT_PATH)) ) {
-		 	exit("XOOPS path check: Script is not inside XOOPS_ROOT_PATH and cannot run.");
-		}
-	}
-
-	// Database
-	// Choose the database to be used
-	define('XOOPS_DB_TYPE', 'mysql');
-
-	// Table Prefix
-	// This prefix will be added to all new tables created to avoid name conflict in the database. Please change this to something different than 'xoops'.
-	define('XOOPS_DB_PREFIX', 'x224');
-
-	// Database Hostname
-	// Hostname of the database server. If you are unsure, 'localhost' works in most cases.
-	define('XOOPS_DB_HOST', 'localhost');
-
-	// Database Username
-	// Your database user account on the host
-	define('XOOPS_DB_USER', 'root');
-
-	// Database Password
-	// Password for your database user account
-	define('XOOPS_DB_PASS', 'skalpa');
-
-	// Database Name
-	// The name of database on the host. The installer will attempt to create the database if not exist
-	define('XOOPS_DB_NAME', 'xoops_dev');
-
-	// Use persistent connection? (Yes=1 No=0)
-	// Default is 'No'. Choose 'No' if you are unsure.
-	define('XOOPS_DB_PCONNECT', '0');
-
-	define('XOOPS_GROUP_ADMIN', '1');
-	define('XOOPS_GROUP_USERS', '2');
-	define('XOOPS_GROUP_ANONYMOUS', '3');
-
-	if (!isset($xoopsOption['nocommon']) && XOOPS_ROOT_PATH != '') {
-		require XOOPS_ROOT_PATH."/include/common.php";
-	}
+// XOOPS is not installed yet.
+if(! defined('XOOPS_INSTALL')){
+    header('Location: install/index.php');
 }
 ?>
