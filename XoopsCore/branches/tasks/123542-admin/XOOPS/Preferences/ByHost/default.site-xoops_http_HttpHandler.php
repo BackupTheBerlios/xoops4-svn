3283075e7603b98719dc070dbcca537572eafffd
skalpa <skalpa@8fd21cda-de02-0410-8884-89147d1a766c>
2006-02-19 19:16:38 +0000

    Added the new folder for managed preferences, with a few examples. (Managed preferences allow admins to lock some settings, ensuring they can't be modified using the configuration module) NB: this feature is still buggy, but will be fixed in alpha 3

---
+++ XoopsCore/branches/tasks/123542-admin/XOOPS/Preferences/ByHost/default.site-xoops_http_HttpHandler.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Example of host-specific managed preferences file...
+ * 
+ * Enter preference values administratively managed in files like this one,
+ * named after the bundleId of the object you want to configure.
+ * 
+ * Host-specific preferences files must be named like hostname-bundleid.php.
+ * As they are read after managed preferences applying to all hosts, you are not
+ * allowed to specify an host-specific setting if it has already been set in the
+ * 'all hosts' domain (inside the main Preferences folder)
+ */
+
+/*
+return array(
+	'fakeRedirectDelay' => 5,
+);
+*/
+return array();
+
+?>
---
+++ XoopsCore/branches/tasks/123542-admin/XOOPS/Preferences/xoops_http_HttpHandler.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Example of managed preferences file...
+ * 
+ * Enter preference values administratively managed in files like this one,
+ * named after the bundleId of the object you want to configure.
+ * 
+ * Managed preferences are not overridable through user interface tools, thus
+ * you can use this system to protect some important settings from modification.
+ */
+
+/*
+return array(
+	'cacheLimiter' => 'public',
+);
+*/
+return array();
+
+?>
